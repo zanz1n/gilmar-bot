5504968c1fbd6d8c4e17f7c6d8e2b1a8a09651e8
Izan Rodrigues <izanrodrigues999@gmail.com>
2023-07-14 01:45:52 -0300

    adding ephemeral response utility builder

--- etc.go
+++ etc.go
@@ -62,6 +62,16 @@ func BasicResponse(format string, args ...any) *discordgo.InteractionResponse {
 	}
 }
 
+func BasicEphemeralResponse(format string, args ...any) *discordgo.InteractionResponse {
+	return &discordgo.InteractionResponse{
+		Type: discordgo.InteractionResponseChannelMessageWithSource,
+		Data: &discordgo.InteractionResponseData{
+			Flags:   discordgo.MessageFlagsEphemeral,
+			Content: fmt.Sprintf(format, args...),
+		},
+	}
+}
+
 func BasicResponseEdit(format string, args ...any) *discordgo.WebhookEdit {
 	fmt := fmt.Sprintf(format, args...)
 	return &discordgo.WebhookEdit{
